dfa9111361bcd0770abbe50142e32f57606bf4ec
Jan K <bx-one-family@outlook.com>
2025-05-04 19:13:07 +0100

    Add main file

---
+++ checkhash.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import argparse
+
+parser = argparse.ArgumentParser(description="CheckHash")
+parser.add_argument("hash", help="The hash to identify")
+
+inputHash = parser.parse_args()
+
+
